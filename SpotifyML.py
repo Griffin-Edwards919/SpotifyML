46057dc0433277b5f8610eb60b5c01a2cf64874e
21edwards <21edwards@da.org>
2021-01-31 12:02:44 -0500

    Imports, shuffles, and separates data

--- SpotifyML.py
+++ SpotifyML.py
@@ -16,36 +16,33 @@ class Song:
     def __str__(self):
         return self.artist + ' ' + self.name + ' ' + self.year + ' ' + self.popularity
 
+def dataIn(file):
+    dataset = open(file)
+    songlist = []
+    for song in csv.reader(dataset):
+        songlist.append(Song(song[1], song[12], song[18], song[5], song[3], song[2], song[9], song[10], song[15], song[13]))
+    dataset.close()
+    shuffled = open('ShuffledData', 'w')
+    random.shuffle(songlist)
+    for song in songlist[:-1]:
+        shuffled.write(song.artist+','+song.name+','+song.year+','+song.isExplicit+','+song.duration+','+song.danceability+','+song.liveness+','+song.loudness+','+song.speechiness+','+song.popularity+'\n')
+    shuffled.write(songlist[-1].artist + ',' + songlist[-1].name + ',' + songlist[-1].year + ',' + songlist[-1].isExplicit + ',' + songlist[-1].duration + ',' + songlist[-1].danceability + ',' + songlist[-1].liveness + ',' + songlist[-1].loudness + ',' + songlist[-1].speechiness + ',' + songlist[-1].popularity)
+    shuffled.close()
+    shuffled = open('ShuffledData', 'r')
+    practice = open('PracticeData', 'w')
+    test = open('TestData', 'w')
+    x = 0
+    for line in shuffled:
+        if x > 139512:
+            test.write(line)
+        else:
+            practice.write(line)
+        x += 1
+    shuffled.close()
+    practice.close()
+    test.close()
 
 if __name__ == '__main__':
-    # Code used to import and shuffle data:
 
-    # dataset = open('data.csv')
-    # songlist = []
-    # for song in csv.reader(dataset):
-    # songlist.append(Song(song[1], song[12], song[18], song[5], song[3], song[2], song[9], song[10], song[15], song[13]))
-    # dataset.close()
-    # shuffled = open('ShuffledData', 'w')
-    # random.shuffle(songlist)
-    # for song in songlist[:-1]:
-    # shuffled.write(song.artist+','+song.name+','+song.year+','+song.isExplicit+','+song.duration+','+song.danceability+','+song.liveness+','+song.loudness+','+song.speechiness+','+song.popularity+'\n')
-    # shuffled.write(songlist[-1].artist + ',' + songlist[-1].name + ',' + songlist[-1].year + ',' + songlist[-1].isExplicit + ',' + songlist[-1].duration + ',' + songlist[-1].danceability + ',' + songlist[-1].liveness + ',' + songlist[-1].loudness + ',' + songlist[-1].speechiness + ',' + songlist[-1].popularity)
-    # shuffled.close()
-
-    #Code used to separate data (80% for teaching, 20% for testing):
-
-    #data = open('ShuffledData', 'r')
-    #practice = open('PracticeData', 'w')
-    #test = open('TestData', 'w')
-    #x = 0
-    #for line in data:
-        #if x > 139512:
-            #test.write(line)
-        #else:
-            #practice.write(line)
-        #x += 1
-    #data.close()
-    #practice.close()
-    #test.close()
-
-
+    #Imports, shuffles, and separates data into practice and testing subsets (80:20 ratio)
+    #dataIn('data.csv')
