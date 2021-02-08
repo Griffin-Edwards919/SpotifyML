5f6da17a26dd53b57487001ee8dacb98f3117d3f
21edwards <21edwards@da.org>
2021-02-08 14:53:23 -0500

    Realized I was doing everything and wrong and basically started over

--- SpotifyML.py
+++ SpotifyML.py
@@ -1,48 +1,86 @@
-import csv, random
+import pandas as pd, numpy
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
-class Song:
-    def __init__(self, a, t, y, e, d, n, l, o, s, p):
-        self.artist = a
-        self.name = t
-        self.year = y
-        self.isExplicit = e
-        self.duration = d
-        self.danceability = n
-        self.liveness = l
-        self.loudness = o
-        self.speechiness = s
-        self.popularity = p
-
-    def __str__(self):
-        return self.artist + ' ' + self.name + ' ' + self.year + ' ' + self.popularity
-
-def dataIn(file):
-    dataset = open(file)
-    songlist = []
-    for song in csv.reader(dataset):
-        songlist.append(Song(song[1], song[12], song[18], song[5], song[3], song[2], song[9], song[10], song[15], song[13]))
-    dataset.close()
-    shuffled = open('ShuffledData', 'w')
-    random.shuffle(songlist)
-    for song in songlist[:-1]:
-        shuffled.write(song.artist+','+song.name+','+song.year+','+song.isExplicit+','+song.duration+','+song.danceability+','+song.liveness+','+song.loudness+','+song.speechiness+','+song.popularity+'\n')
-    shuffled.write(songlist[-1].artist + ',' + songlist[-1].name + ',' + songlist[-1].year + ',' + songlist[-1].isExplicit + ',' + songlist[-1].duration + ',' + songlist[-1].danceability + ',' + songlist[-1].liveness + ',' + songlist[-1].loudness + ',' + songlist[-1].speechiness + ',' + songlist[-1].popularity)
-    shuffled.close()
-    shuffled = open('ShuffledData', 'r')
-    practice = open('PracticeData', 'w')
-    test = open('TestData', 'w')
-    x = 0
-    for line in shuffled:
-        if x > 139512:
-            test.write(line)
-        else:
-            practice.write(line)
-        x += 1
-    shuffled.close()
-    practice.close()
-    test.close()
+def func(x, a, b, c):
+    return a * numpy.exp(-b * x) + c
 
 if __name__ == '__main__':
 
-    #Imports, shuffles, and separates data into practice and testing subsets (80:20 ratio)
-    #dataIn('data.csv')
+    #importing data
+    df = pd.read_csv('data.csv')
+
+    #stripping data of non-numeric values
+    df = df.drop(['artists', 'id', 'key', 'mode', 'release_date', 'name'], axis=1)
+
+    #measuring correlation of the variables to the target variable
+    corr = df.corr()
+    print(corr['popularity'])
+
+    #stripping data of any very weakly correlated variables
+    df = df.drop(['duration_ms', 'liveness', 'tempo', 'valence'], axis=1)
+
+    #separating data at 80-20 ratio and creating X and Y sets for the 8 independant variables and 1 dependant
+    X_train, X_test = train_test_split(df, test_size=0.2)
+    Y_train = X_train['popularity']
+    Y_test = X_test['popularity']
+    X_train = X_train.drop(['popularity'], axis=1)
+    X_test = X_test.drop(['popularity'], axis=1)
+
+    #creating least squares model
+    model = linear_model.LinearRegression()
+    model.fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+
+    #printing model success
+    print("\n\n____Least Squares Model____")
+    print('Coefficients:', model.coef_)
+    print('Intercept:', model.intercept_)
+    print('Mean squared error:', mean_squared_error(Y_test, Y_pred))
+    print('R^2:', r2_score(Y_test, Y_pred))
+    plt.scatter(Y_test, Y_pred, marker='+', alpha=.5)
+    plt.show()
+
+    # creating ridge model
+    model = linear_model.Ridge()
+    model.fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+
+    # printing model success
+    print("\n\n____Ridge Model____")
+    print('Coefficients:', model.coef_)
+    print('Intercept:', model.intercept_)
+    print('Mean squared error:', mean_squared_error(Y_test, Y_pred))
+    print('R^2:', r2_score(Y_test, Y_pred))
+    plt.scatter(Y_test, Y_pred, marker='+', alpha=.5)
+    plt.show()
+
+    #creating lasso model
+    model = linear_model.Lasso()
+    model.fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+
+    # printing model success
+    print("\n\n____Lasso Model____")
+    print('Coefficients:', model.coef_)
+    print('Intercept:', model.intercept_)
+    print('Mean squared error:', mean_squared_error(Y_test, Y_pred))
+    print('R^2:', r2_score(Y_test, Y_pred))
+    plt.scatter(Y_test, Y_pred, marker='+', alpha=.5)
+    plt.show()
+
+    # creating elastic-net model
+    model = linear_model.ElasticNet()
+    model.fit(X_train, Y_train)
+    Y_pred = model.predict(X_test)
+
+    # printing model success
+    print("\n\n____Elastic-Net Model____")
+    print('Coefficients:', model.coef_)
+    print('Intercept:', model.intercept_)
+    print('Mean squared error:', mean_squared_error(Y_test, Y_pred))
+    print('R^2:', r2_score(Y_test, Y_pred))
+    plt.scatter(Y_test, Y_pred, marker='+', alpha=.5)
+    plt.show()
